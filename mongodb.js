a443bfadc7c360f7746f21df0eaacc47f1521bf0
daniel <daniel@workpay.co.ke>
2019-11-11 17:18:12 +0300

    insert data in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,17 +1,44 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
 
 const databaseUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(databaseUrl,{useUnifiedTopology: true},(error,client)=>{
   if(error){
       return console.log('Unable to connect to database');
   }
   const db = client.db(databaseName);
-  db.collection('users').insertOne({
-      name:'Daniel',
-      age:26
-  })
+  // db.collection('users').insertOne({
+  //     name:'Daniel',
+  //     age:26
+  // },(error,result)=>{
+  //   if(error){
+  //    return console.log('error connecting');
+  //   }
+  //   console.log(result.ops);
+   
+  // })
+  // db.collection('tasks').insertMany([
+  //   {
+  //     description:'test',
+  //     completed:false
+  // },{
+  //   description:'It again',
+  //   completed:true
+  // },
+  // {
+  //   description:'doing it',
+  //   completed:true
+  // }
+  // ],(error,result)=>{
+  //   if(error){
+  //     return console.log('insert Many failed')
+  //   }
+  //   console.log(result.ops)
+  // })
   client.close();
 });
